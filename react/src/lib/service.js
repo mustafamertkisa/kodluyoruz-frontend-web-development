ef0a0bd4c19684effee66dd4d333f19c211f9373
mustafamertkisa <m.mertkisa@gmail.com>
2021-09-12 19:06:06 +0300

    react temelleri ödev 1 yapıldı

---
+++ react/src/lib/service.js
@@ -0,0 +1,13 @@
+const { default: axios } = require("axios");
+
+const getData = async (id) => {
+
+    const {data: users} = await axios("https://jsonplaceholder.typicode.com/users/"+id);
+    const {data: posts} = await axios("https://jsonplaceholder.typicode.com/posts?userId="+id);
+
+    console.log(users);
+    console.log(posts);
+}
+
+
+export default getData;
